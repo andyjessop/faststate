a747a3b039bcb8039aceae79442744a2bcdb31ff
Andy Jessop <andy@andyjessop.com>
2018-02-28 17:03:19 +0100

    Remove extra loop from function getter

--- src/computed/get-fns-to-run.js
+++ src/computed/get-fns-to-run.js
@@ -1,9 +1,14 @@
 import removeDuplicates from '../utils/remove-duplicates';
 
 export default (depChains, resolvedDepGraph, fns, data) => removeDuplicates(Object.keys(data)
-  .reduce((acc, cur) => acc.concat(depChains[cur]), []))
+  .reduce((acc, cur) => {
+    if (depChains[cur]) {
+      return acc.concat(depChains[cur]);
+    }
+
+    return acc;
+  }, []))
   .sort((a, b) => resolvedDepGraph.indexOf(a) - resolvedDepGraph.indexOf(b))
-  .filter(dep => fns[dep])
   .map(dep => ({
     prop: dep,
     fn: fns[dep],
